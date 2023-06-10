d2b9162a176b3f9038ed56cde5a755b824313ad0
Bruno Alves <balves16@hotmail.com>
2023-06-10 09:59:16 +0200

    remove server dependence

--- bin/MadGraph5_aMCatNLO/htcondor/gridpacks/submission.sh
+++ bin/MadGraph5_aMCatNLO/htcondor/gridpacks/submission.sh
@@ -6,35 +6,21 @@ LAMBDA112=${3}
 KAPPA111=${4}
 OUTDIR=${5}
 CARDDIR=${6}
-SERVER=${7}
 
-if [[ ${SERVER} == "llr" ]]; then
-	BASE="/home/llr/cms/alves/genproductions/bin/MadGraph5_aMCatNLO"
-	STORAGE="/data_CMS/llr/cms/alves/FiniteWidth/"
-elif [[ ${SERVER} == "lxplus" ]]; then
-	BASE="/afs/cern.ch/work/${USER:0:1}/${USER}/genproductions/bin/MadGraph5_aMCatNLO"
-	STORAGE="/eos/user/${USER:0:1}/${USER}/FiniteWidth/"
-else
-	echo "Server error."
-	exit 1
-fi
-
-PARS="ST${STHETA}_L${LAMBDA112}_K${KAPPA111}_M${MASS}"
-NAME="Singlet_hh_${PARS}"
-
-JOBDIR=${PWD}
+BASE="/afs/cern.ch/work/${USER:0:1}/${USER}/genproductions/bin/MadGraph5_aMCatNLO"
+NAME="Singlet_hh_M${MASS}_ST${STHETA}_L${LAMBDA112}_K${KAPPA111}"
 
 cd ${BASE}
 
 PYTHONPATH=$PYTHONPATH:/usr/lib64/python3.6/site-packages/; \
-	./gridpack_generation.sh ${NAME} cards/production/13TeV/${CARDDIR}/${NAME}/ ${JOBDIR} ${SERVER}
+	./gridpack_generation.sh ${NAME} cards/production/13TeV/${CARDDIR}/${NAME}/
 
-# mv ${STORAGE}/${NAME} ${OUTDIR}
-# mv ${NAME}.log ${OUTDIR}
-# mv ${NAME}_slc7_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz ${OUTDIR} 
+mv ${BASE}/${NAME} ${OUTDIR}
+mv ${NAME}.log ${OUTDIR}
+mv ${NAME}_slc7_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz ${OUTDIR} 
 
-# mv ${JOBDIR}/"${NAME}_job.err" ${OUTDIR}/${NAME}
-# mv ${JOBDIR}/"${NAME}_job.out" ${OUTDIR}/${NAME}
-# mv ${JOBDIR}/"${NAME}.log" ${OUTDIR}/${NAME}
+mv "${NAME}_job.err" ${OUTDIR}/${NAME}
+mv "${NAME}_job.out" ${OUTDIR}/${NAME}
+mv "${NAME}.log" ${OUTDIR}/${NAME}
 
 echo "Done!"
--- bin/MadGraph5_aMCatNLO/htcondor/gridpacks/submission_generator.py
+++ bin/MadGraph5_aMCatNLO/htcondor/gridpacks/submission_generator.py
@@ -8,25 +8,20 @@ def ntos(n, around=None):
         n = np.round(n, around)
     return str(n).replace('.', 'p').replace('-', 'm')
 
-example = 'python generateCards.py --out TestSinglet --template SingletModel/cards_templates/'
+example = 'python generateCards.py --out TestSinglet --template SingletModel/cards_templates/ --tag FullScanV1'
 parser = argparse.ArgumentParser(description='Plotter for finite width studies.\nExample: {} .'.format(example))
 parser.add_argument("--out_dir", required=True, help="Output directory.",)
+parser.add_argument("--tag", required=True, help="Identifier. Needs to match the datacards' tag.")
 parser.add_argument("--card_dir", required=True,
                     choices=('Singlet_resonly', 'Singlet_nores', 'Singlet_all'), 
                     help="Datacards subdirectory.",)
-parser.add_argument("--server", default='llr', choices=('llr', 'lxplus'),
-                    help="Server where the script is running.",)
 FLAGS = parser.parse_args()
 
-base_local = 'genproductions/bin/MadGraph5_aMCatNLO/htcondor/gridpacks/'
-if FLAGS.server == 'llr':
-    base_local = os.path.join('/home/llr/cms/alves/', base_local)
-    base_storage = os.path.join('/data_CMS/cms/alves/FiniteWidth/')
-elif FLAGS.server == 'lxplus':
-    base_local = os.path.join('/afs/cern.ch/work/',
-                              os.environ['USER'][0], os.environ['USER'], base_local)
-    base_storage = os.path.join('/eos/user/',
-                                os.environ['USER'][0], os.environ['USER'], 'FiniteWidth')
+base_local = os.path.join('/afs/cern.ch/work/',
+                          os.environ['USER'][0], os.environ['USER'],
+                          'genproductions/bin/MadGraph5_aMCatNLO/htcondor/gridpacks/')
+base_storage = os.path.join('/eos/user/',
+                            os.environ['USER'][0], os.environ['USER'], 'FiniteWidth')
 
 out_dir = os.path.join(base_storage, FLAGS.out_dir + '/')
 
@@ -38,32 +33,31 @@ for d in (out_dir,):
         mes += 'Deletion command: rm -r {}\n'.format(d)
         raise RuntimeError(mes)
 
-# masses = ('250', '260', '270', '280', '300', '320', '350', '400',
-#           '450', '500', '550', '600', '650', '700', '750', '800',
-#           '850', '900', '1000', '1250', '1500', '1750', '2000', '2500', '3000')    
-mass_points = (250, 350, 450, 550, 650, 750, 850, 950)
-stheta_points = np.arange(0.,1.1,.2) # sine of theta mixing between the new scalar and the SM Higgs
-l112_points = np.arange(-300,301,100) # resonance coupling with two Higgses
-k111_points = (1.,) #np.arange(-7,12) # tri-linear kappa
+# masses = ('250', '270', '300', '350', '400',
+#           '450', '500', '600', '700', '800', '900', '1000')
+masses = ('250',)
+sthetas = np.arange(0.,1.0001,.5) # sine of theta mixing between the new scalar and the SM Higgs
+lambdas = np.arange(-300,301,300) # resonance coupling with two Higgses
+kappas = (1.0,)# 2.4, 10.0)
 
 def add_new_line(m, st, lbd, kap, s):
-    if not (m==mass_points[-1] and st==stheta_points[-1] and lbd==l112_points[-1] and kap==k111_points[-1]):
+    if not (m==masses[-1] and st==sthetas[-1] and lbd==lambdas[-1] and kap==kappas[-1]):
         return s + "\n"
     else:
         return s
 
 loop_inside = ''
-for st in stheta_points:
-    for lbd in l112_points:
-        for kap in k111_points:
-            for m in mass_points:
+for st in sthetas:
+    for lbd in lambdas:
+        for kap in kappas:
+            for m in masses:
                 loop_inside += '    ' + ntos(m) + ', ' + ntos(st, 1) + ', ' + ntos(lbd) + ', ' + ntos(kap)
                 loop_inside = add_new_line(m, st, lbd, kap, loop_inside)
 
-outfile = "Singlet_hh_ST$(Stheta)_L$(Lambda112)_K$(Kappa111)_M$(Mass)"
+outfile = "Singlet_" + FLAGS.tag + "_M$(Mass)_ST$(Stheta)_L$(Lambda112)_K$(Kappa111)"
 m = ( 'universe = vanilla',
       'executable = ' + os.path.join(base_local, 'submission.sh'),
-      'arguments  = $(Mass) $(Stheta) $(Lambda112) $(Kappa111) {} {} {}'.format(out_dir, FLAGS.card_dir, FLAGS.server),
+      'arguments  = $(Mass) $(Stheta) $(Lambda112) $(Kappa111) {} {}'.format(out_dir, FLAGS.card_dir),
       'output     = ' + outfile + '_job.out',
       'error      = ' + outfile + '_job.err',
       #'log        = ' + outfile + '_job.log',
@@ -73,8 +67,10 @@ m = ( 'universe = vanilla',
       '+JobFlavour   = "microcentury"', # 1 hour (see https://batchdocs.web.cern.ch/local/submit.html)
 
       'RequestCpus   = 1',
-      'RequestMemory = 1GB',
-      'RequestDisk   = 512MB',
+      'RequestMemory = 4GB',
+      'RequestDisk   = 2GB',
+
+      'max_materialize = 50',
       
       'queue Mass, Stheta, Lambda112, Kappa111 from (',
       loop_inside,
