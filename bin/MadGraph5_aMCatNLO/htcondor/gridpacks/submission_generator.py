74e4245d6b2d3712408551c4f6374c83632ccdbe
Bruno Alves <balves16@hotmail.com>
2023-08-10 20:21:10 +0200

    add manual option

--- bin/MadGraph5_aMCatNLO/htcondor/gridpacks/submission_generator.py
+++ bin/MadGraph5_aMCatNLO/htcondor/gridpacks/submission_generator.py
@@ -5,12 +5,19 @@ import argparse
 import numpy as np
 from dataclasses import dataclass
 
-@dataclass
-class ParScan:
-    mas: list[float]
-    sth: np.ndarray  # sine of theta mixing between the new scalar and the SM Higgs
-    lbd: np.ndarray  # resonance coupling with two Higgses
-    kap: tuple[float]
+class ScanParameters:
+    class dot(dict):
+        """dot.notation access to dictionary attributes"""
+        __getattr__ = dict.get
+        __setattr__ = dict.__setitem__
+        __delattr__ = dict.__delitem__
+
+    def __init__(self, masses: list[float], sthetas: list[float],
+                 lambdas: list[float], kappas: list[float]):
+        self.masses  = mass
+        self.sthetas = stheta
+        self.lambdas = lambda112
+        self.kappas  = kappa111
 
 @dataclass
 class Pathes:
@@ -74,46 +81,60 @@ def get_condor_submission_main_text(path, job_name, child):
         m += '\nqueue Mass, Stheta, Lambda112, Kappa111 from (\n'
     return m
 
-def write_condor_file(par, path, tag, child):
+def write_condor_file(par, path, tag, manual, child=True):
     """Write condor submission file."""
-    for st in par.sth:
-        for lbd in par.lbd:
-            for kap in par.kap:
-                for m in par.mas:
-                    condor_name = ("Singlet_T" + tag + "_M" + ntos(m) + "_ST" + ntos(st,1) +
-                                   "_L" + ntos(lbd) + "_K" + ntos(kap))
-                    if child:
-                        mes = get_condor_submission_main_text(path, condor_name, child=True)
-                        with open(os.path.join(path.condor, tag, condor_name + '_child.condor'), 'w') as file:
-                            file.write(mes)
+    def single(st, lbd, kap, m):
+        condor_name = ("Singlet_T" + tag + "_M" + ntos(m) + "_ST" + ntos(st,1) +
+                       "_L" + ntos(lbd) + "_K" + ntos(kap))
+        if child:
+            mes = get_condor_submission_main_text(path, condor_name, child=True)
+            with open(os.path.join(path.condor, tag, condor_name + '_child.condor'), 'w') as file:
+                file.write(mes)
+                
+                mes = get_condor_submission_main_text(path, condor_name, child=False)
+                with open(os.path.join(path.condor, tag, condor_name + '.condor'), 'w') as file:
+                    full = mes + '    ' + ntos(m) + ', ' + ntos(st, 1) + ', ' + ntos(lbd) + ', ' + ntos(kap) + '\n)'
+                    file.write(full)
 
-                    mes = get_condor_submission_main_text(path, condor_name, child=False)
-                    with open(os.path.join(path.condor, tag, condor_name + '.condor'), 'w') as file:
-                        full = mes + '    ' + ntos(m) + ', ' + ntos(st, 1) + ', ' + ntos(lbd) + ', ' + ntos(kap) + '\n)'
-                        file.write(full)
+    if manual:
+        for m,st,kap,lbd in zip(par.masses,par.sthetas,par.kappas,par.lambdas):
+            single(st, lbd, kap, m)
+    else:            
+        for st in par.sthetas:
+            for lbd in par.lambdas:
+                for kap in par.kappas:
+                    for m in par.masses:
+                        single(st, lbd, kap, m)
 
         
-def write_dag_file(par, path, tag, child=True):
+def write_dag_file(par, path, tag, manual, child=True):
     """Write condor DAGMAN submission file."""
-    file = open(os.path.join(path.baselocal, 'submission_' + tag + '.dag'), 'w')
+    def single():
+        job_name = "Singlet_T" + tag + "_M" + ntos(m) + "_ST" + ntos(st,1) + "_L" + ntos(lbd) + "_K" + ntos(kap)
+        m = 'JOB {}_job {}/{}/{}.condor \n'.format(job_name, path.condor, tag, job_name)
+        if child:
+            m += 'JOB {}_child {}/{}/{}_child.condor \n'.format(job_name, path.condor, tag, job_name)
+            m += 'PARENT {j}_job CHILD {j}_child '.format(j=job_name) + '\n\n'
+        else:
+            move_args = ' '.join((path.out, tag, job_name))
+            m += 'SCRIPT POST {}_job {}/move.sh '.format(job_name, path.baselocal) + move_args + '\n\n'
+        file.write(m)
 
+    file = open(os.path.join(path.baselocal, 'submission_' + tag + '.dag'), 'w')
     file.write('CONFIG {}/dag.config\n\n'.format(path.baselocal))
     
-    for st in par.sth:
-        for lbd in par.lbd:
-            for kap in par.kap:
-                for m in par.mas:
-                    job_name = "Singlet_T" + tag + "_M" + ntos(m) + "_ST" + ntos(st,1) + "_L" + ntos(lbd) + "_K" + ntos(kap)
-                    m = 'JOB {}_job {}/{}/{}.condor \n'.format(job_name, path.condor, tag, job_name)
-                    if child:
-                        m += 'JOB {}_child {}/{}/{}_child.condor \n'.format(job_name, path.condor, tag, job_name)
-                        m += 'PARENT {j}_job CHILD {j}_child '.format(j=job_name) + '\n\n'
-                    else:
-                        move_args = ' '.join((path.out, tag, job_name))
-                        m += 'SCRIPT POST {}_job {}/move.sh '.format(job_name, path.baselocal) + move_args + '\n\n'
-                    file.write(m)
+    if manual:
+        for m,st,kap,lbd in zip(par.masses,par.sthetas,par.kappas,par.lambdas):
+            single(st, lbd, kap, m)
+    else:            
+        for st in par.sthetas:
+            for lbd in par.lambdas:
+                for kap in par.kappas:
+                    for m in par.masses:
+                        single(st, lbd, kap, m)
+
     file.close()
-    
+
 if __name__ == "__main__":
     major, minor, _, _, _ = sys.version_info
     if major < 3 or (major == 3 and minor < 9):
@@ -128,6 +149,8 @@ if __name__ == "__main__":
                         choices=('Singlet_resonly', 'Singlet_nores', 'Singlet_all'), 
                         help="Datacards subdirectory.",)
     parser.add_argument('--no_child', action='store_true')
+    manual_help = "Produce data points as individually specified (no scan across all combinations)."
+    parser.add_argument("--manual", action="store_true", help=manual_help)
     FLAGS = parser.parse_args()
 
     base_local = os.path.join('/afs/cern.ch/work/',
@@ -143,12 +166,21 @@ if __name__ == "__main__":
 
     out_dir = os.path.join(base_storage, FLAGS.out_dir + '/')
     create_dir(out_dir)
-    
-    pars = ParScan(mas=('250', '270', '300', '350', '400',
-                        '500', '600', '700', '800', '900', '1000'),
-                   sth=np.arange(0.,1.001,.1),
-                   lbd=np.arange(-300,301,100),
-                   kap=(1.0,2.4,10.0))
+
+    if FLAGS.manual:
+        mass_points = (300.00, 300.00, 600.00, 600.00, 1000.00, 1000.00)             
+        stheta_points = (0.3, 0.1, 0.8, 0.1, 0.4, 0.3)
+        l112_points = (-500.00, -400.00, 600.00, 300.00, 100.00, 300.00)
+        k111_points = (1.0,)*6 # + (2.4,)*6 + ... tri-linear kappa
+    else:
+        mass_points = (280.00, 300.00, 400.00, 500.00, 600.00, 700.00, 800.00, 900.00, 1000.00)
+        stheta_points = (0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99) # sine of theta mixing between the new scalar and the SM Higgs
+        l112_points = (-600.00, -500.00, -400.00, -300.00, -200.00, -100.00, -50.00,
+                       0.00, 50.00, 100.00, 200.00, 300.00, 400.00, 500.00, 600.00) # resonance coupling with two Higgses
+        k111_points = (-1.0, 1.0, 2.4, 6.0) # tri-linear kappa
+
+    pars = ScanParameters(masses=mass_points, sthetas=stheta_points,
+                          lambdas=lbd112_points, kappas=k111_points)
 
     pathes = Pathes(baselocal=base_local,
                     basestorage=base_storage,
